6226841ebba7a9f4bd42acc3463f3c4048aea635
Taylor Wills <taylorwills@Taylors-MacBook-Pro.local>
2024-06-03 15:14:07 -0400

    Building a passing thoughts project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import HelloWorld from './HelloWorld';
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { AddThoughtForm } from './AddThoughtForm';
+import { Thought } from './Thought';
+import { generateId, getNewExpirationTime } from './utilities';
 
-function App() {
+export default function App() {
+
+  const [thoughts, setThoughts] = useState([
+    {
+      id: generateId(),
+      text: "This is a place for your passing thoughts.",
+      expiresAt: getNewExpirationTime(),
+    },
+    {
+      id: generateId(),
+      text: "They'll be removed after 15 seconds.",
+      expiresAt: getNewExpirationTime(),
+    },
+  ]);
+
+  const addThought = (thought) => {
+    setThoughts((thoughts) => [thought, ...thoughts]);
+  };
+
+  const removeThought = (thoughtIdToRemove) => {
+    setThoughts((thoughts) =>
+      thoughts.filter((thought) => thought.id !== thoughtIdToRemove)
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,10 +38,14 @@ function App() {
         <HelloWorld />
       </header>
       <main>
-        <h2>A Clean Starting Point!</h2>
+        <h2>💭 Passing Thoughts</h2>
+        <AddThoughtForm addThought={addThought} />
+        <ul className="thoughts">
+          {thoughts.map((thought) => (
+            <Thought key={thought.id} thought={thought} removeThought={removeThought} />
+          ))}
+        </ul>
       </main>
     </div>
   );
 }
-
-export default App;
