6ef9f967d1d46bf7559ea7110960c7e972d91216
Taylor Wills <taylorwills@Taylors-MacBook-Pro.local>
2024-06-04 12:13:47 -0400

    Styling react with inline styles and style objects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const myStyle = {
+    background: 'lightblue',
+    color: 'darkblue'
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +15,8 @@ function App() {
         <HelloWorld />
       </header>
       <main>
-        <h2>A Clean Starting Point!</h2>
+        <h2 style={myStyle}>A Clean Starting Point!</h2>
+        <p style={{ background: 'lightgreen', color: 'darkgreen' }}>This is styled with inline styles</p>
       </main>
     </div>
   );
